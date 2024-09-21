0e1711b9d1bbccb3b2c5dcce97d4d421cd2d2b94
Akyoto1 <119670345+Akyoto1@users.noreply.github.com>
2024-09-21 00:00:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+var studentName = "Sarah"; //declaring student's name
+var studentAge = "19"; // declaring the student's age
+var isEnrolled = "True"; // declaring student's enrollment
+
+console.log(studentName);
+console.log(studentAge);
+console.log(isEnrolled);
+
+let personAddress = "77 Baker St. W";
+let personWeight = "180.5"; 
+let lostWeight = "true";
+let favoriteColors = ['orange', 'yellow', 'black'];
+let person = {firstName: "Jimmy", lastName: "Storm", age: 25};
+
+console.log(personAddress);
+console.log(personWeight);
+console.log(lostWeight);
+console.log(favoriteColors);
+console.log(person);
+
+console.log(5*3/7+200-22); //Math
+console.log(77+30);
+console.log(92-90);
+console.log(7*19);
+console.log(22/3);
+
+let a = 3
+let b = 9
+console.log(a >=b); // Greater than
+console.log(a <=b); // Less than
+console.log("9" == a); // Equal to
+console.log(a !== b); // Not Equal 
+
+let x = true;
+let y = false;
+console.log(x && y); // Logical AND
+console.log(x || y); // Logical OR
+console.log(!x); // Logical NOT
